f7c897c73e1cd086650d5ad0c76c53ded1fb28b7
eamsdev <76290232+pete-eams@users.noreply.github.com>
2023-03-08 22:36:58 +1100

    remove redundant resources

--- modules/api_gateway/main.tf
+++ modules/api_gateway/main.tf
@@ -51,28 +51,6 @@ resource "aws_api_gateway_integration" "main" {
   }
 }
 
-resource "aws_api_gateway_method_response" "main" {
-  rest_api_id = aws_api_gateway_rest_api.main.id
-  resource_id = aws_api_gateway_resource.main.id
-  http_method = aws_api_gateway_method.main.http_method
-  status_code = "200"
-}
-
-resource "aws_api_gateway_integration_response" "main" {
-  rest_api_id = aws_api_gateway_rest_api.main.id
-  resource_id = aws_api_gateway_resource.main.id
-  http_method = aws_api_gateway_method.main.http_method
-  status_code = aws_api_gateway_method_response.main.status_code
-
-  response_templates = {
-    "application/json" = ""
-  }
-
-  depends_on = [
-    aws_api_gateway_integration.main
-  ]
-}
-
 resource "aws_api_gateway_deployment" "main" {
   depends_on  = [aws_api_gateway_integration.main]
   rest_api_id = aws_api_gateway_rest_api.main.id
